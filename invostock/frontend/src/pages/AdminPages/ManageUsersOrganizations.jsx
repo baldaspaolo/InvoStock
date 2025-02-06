95556f9cd4907e1dce5e4b662be429f63b8833c2
baldaspaolo <101393897+baldaspaolo@users.noreply.github.com>
2025-02-06 14:51:12 +0100

    pages fix

---
+++ invostock/frontend/src/pages/AdminPages/ManageUsersOrganizations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManageUsersOrganizations = () => {
+  return <div>MyComponent</div>;
+};
+
+export default ManageUsersOrganizations;
---
+++ invostock/frontend/src/pages/AdminPages/SystemSettings.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SystemSettings = () => {
+  return <div>MyComponent</div>;
+};
+
+export default SystemSettings;
