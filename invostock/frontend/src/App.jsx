4aaef604a014bd7b0cbbd163f6fcf7d1d7f074de
baldaspaolo <101393897+baldaspaolo@users.noreply.github.com>
2025-02-22 21:50:44 +0100

    added user state
    -added AuthContext and localStorage

--- invostock/frontend/src/App.jsx
+++ invostock/frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { Toast } from "primereact/toast";
+
 import "./App.css";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -12,19 +15,22 @@ import Invoices from "./pages/UserOrganizationPages/Invoices";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+    <AuthProvider>
+      <Router>
+        <Toast ref={(el) => (window.toast = el)} />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/invoices" element={<Invoices />} />
-        </Route>
+          <Route element={<Layout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/invoices" element={<Invoices />} />
+          </Route>
 
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </Router>
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
