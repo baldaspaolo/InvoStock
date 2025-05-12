2d1e1f9cc8c5786448e2a1d65a91307889febf20
baldaspaolo <101393897+baldaspaolo@users.noreply.github.com>
2025-05-12 11:34:41 +0200

    removed unused pages

--- invostock/frontend/src/pages/AdminPages/ManageUsersOrganizations.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ManageUsersOrganizations = () => {
-  return <div>MyComponent</div>;
-};
-
-export default ManageUsersOrganizations;
--- invostock/frontend/src/pages/AdminPages/SystemSettings.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SystemSettings = () => {
-  return <div>MyComponent</div>;
-};
-
-export default SystemSettings;
